56bc06ba859713db1e5a74cf84d2964a681faf24
François Kooman <fkooman@tuxed.net>
2019-08-20 21:45:10 +0200

    fix typo

--- locale/nl_NL.php
+++ locale/nl_NL.php
@@ -2,5 +2,5 @@
 
 return [
     'Other Devices...' => 'Andere apparaten...',
-    'Use the offical eduVPN applications for connecting to eduVPN. You can download them below!' => 'Gebruik de officiele eduVPN applicaties om te verbinden met eduVPN. Download ze hier hieronder!',
+    'Use the offical eduVPN applications for connecting to eduVPN. You can download them below!' => 'Gebruik de officiële eduVPN applicaties om te verbinden met eduVPN. Download ze hier hieronder!',
 ];
