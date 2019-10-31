ab7ef712df4c8bbb19c873e4029a1f182b58f7b8
François Kooman <fkooman@tuxed.net>
2019-10-31 11:06:59 +0100

    add empty translation

---
+++ locale/empty.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2019, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Continue...' => '',
+    'Download and/or install the eduVPN application for your device as found on the "Home" screen;' => '',
+    'Getting Started' => '',
+    'If you have any problems or questions, please contact <a href="mailto:eduvpn-support@lists.geant.org">eduvpn-support@lists.geant.org</a>.' => '',
+    'In order to start using eduVPN, follow the steps below.' => '',
+    'Launch the eduVPN application and follow the instructions.' => '',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => '',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => '',
+    'To use eduVPN, download the app for your device below!' => '',
+    'Welcome to eduVPN!' => '',
+];
