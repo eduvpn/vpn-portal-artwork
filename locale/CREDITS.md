899ff54ed474828feae57e5f7078ccf94e081e91
François Kooman <fkooman@tuxed.net>
2019-10-31 18:10:43 +0100

    add locale for ar_MA and CREDITS

---
+++ locale/CREDITS.md
@@ -0,0 +1,4 @@
+# Credits
+
+* `ar_MA`: Sami Ait Ali Oulahcen (MARWAN)
+* `nl_NL`: François Kooman (DeiC)
---
+++ locale/ar_MA.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2019, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Continue...' => 'واصل...',
+    'Download and/or install the eduVPN application for your device as found on the "Home" screen;' => 'حمل و/او نزل تطبيق eduVPN على جهازك كما هو موجود على "الصفحة الرئيسية"؛',
+    'Getting Started' => 'ابدء',
+    'If you have any problems or questions, please contact <a href="mailto:eduvpn-support@lists.geant.org">eduvpn-support@lists.geant.org</a>.' => 'إذا كان لديك أي مشاكل أو أسئلة، اتصل من فضلك بـ <a href="mailto:eduvpn-support@lists.geant.org">eduvpn-support@lists.geant.org</a>.',
+    'In order to start using eduVPN, follow the steps below.' => 'للبدء باستعمال eduVPN، اتبع الخطوات التالية.',
+    'Launch the eduVPN application and follow the instructions.' => 'افتح تطبيق eduVPN و اتبع التعليمات.',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'على صفحة "الاعدادات"، يمكن للمستعملين المتمرسين تحميل اعدادات الشبكة الخاصة الافتراضية يدويا.',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'على صفحة "حساب" تستطيع منع الولوج الى الشبكة الخاصة الافتراضية اذا فقدت جهازا ما.',
+    'To use eduVPN, download the app for your device below!' => 'لاستعمال eduVPN، حمل التطبيق على جهازك أسفله!',
+    'Welcome to eduVPN!' => 'مرحبا بك على eduVPN!',
+];
