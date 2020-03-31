aae6600ce74d47b1a4bfdf8f57c972822ac52e54
François Kooman <fkooman@tuxed.net>
2020-03-31 16:24:44 +0200

    more pt_PT fixes

--- locale/pt_PT.php
+++ locale/pt_PT.php
@@ -10,13 +10,13 @@
 return [
     'Continue...' => 'Continuar...',
     'Download' => 'Descarregar',
-    'Download and/or install the eduVPN application for your device as found on the "Home" page;' => 'Descarregue e/ou instale a aplicação eduVPN adequada ao seu dispositivo, encontrada na página "Início"',
+    'Download and/or install the eduVPN application for your device as found on the "Home" page;' => 'Descarregue e/ou instale a aplicação eduVPN adequada ao seu dispositivo, encontrada na página "Início";',
     'Getting Started' => 'Começando',
-    'If you have any problems or questions, please contact your institute.' => 'Se tiver algum problema ou dúvida, por favor contate a sua instituição',
-    'In order to start using eduVPN, follow the steps below.' => 'Para começar a usar a eduVPN, siga os passos abaixo',
+    'If you have any problems or questions, please contact your institute.' => 'Se tiver algum problema ou dúvida, por favor contate a sua instituição.',
+    'In order to start using eduVPN, follow the steps below.' => 'Para começar a usar a eduVPN, siga os passos abaixo.',
     'Launch the eduVPN application and follow the instructions.' => 'Inicie a aplicação eduVPN e siga as instruções.',
     'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'Na página "Configurações", utilizadores avançados podem descarregar manualmente as configurações VPN.',
-    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'Na sua página "Conta", pode bloquear acessos VPN caso perca um dispositivo',
-    'To use eduVPN, download the app for your device below!' => 'Para usar o eduVPN, descarregue abaixo, a APP para o seu dispositivo',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'Na sua página "Conta", pode bloquear acessos VPN caso perca um dispositivo.',
+    'To use eduVPN, download the app for your device below!' => 'Para usar o eduVPN, descarregue abaixo, a aplicação para o seu dispositivo.',
     'Welcome to eduVPN!' => 'Bem-vindo ao eduVPN!',
 ];
