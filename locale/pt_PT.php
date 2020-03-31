f2a8dbf1be70938370a28b5b52df7ce7ae5fd730
François Kooman <fkooman@tuxed.net>
2020-03-31 10:07:46 +0200

    add pt_PT translation

---
+++ locale/pt_PT.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2019, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Continue...' => 'Continuar...',
+    'Download' => 'Descarregar',
+    'Download and/or install the eduVPN application for your device as found on the "Home" page;' => 'Descarregue e/ou instale a aplicação eduVPN adequada ao seu dispositivo, encontrada na página "Home"',
+    'Getting Started' => 'Começando',
+    'If you have any problems or questions, please contact <a href="mailto:eduvpn-support@lists.geant.org">eduvpn-support@lists.geant.org</a>.' => 'Se tiver algum problema ou dúvida, por favor contate <a href="mailto:eduvpn-support@lists.geant.org">eduvpn-support@lists.geant.org</a>.',
+    'In order to start using eduVPN, follow the steps below.' => 'Para começar a usar a eduVPN, siga os passos abaixo',
+    'Launch the eduVPN application and follow the instructions.' => 'Inicie a aplicação eduVPN e siga as instruções.',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'Na página "Configurações", utilizadores avançados podem descarregar manualmente as configurações VPN.',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'Na sua página "Conta", pode bloquear acessos VPN caso perca um dispositivo',
+    'To use eduVPN, download the app for your device below!' => 'Para usar o eduVPN, descarregue abaixo, a APP para o seu dispositivo',
+    'Welcome to eduVPN!' => 'Bem-vindo ao eduVPN!',
+];
