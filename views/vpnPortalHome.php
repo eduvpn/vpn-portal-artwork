fce06d37c540f287078ae3681721ad302d702948
François Kooman <fkooman@tuxed.net>
2020-06-08 16:31:54 +0200

    fix apps buttons

--- views/vpnPortalHome.php
+++ views/vpnPortalHome.php
@@ -3,31 +3,30 @@
 <?php if ($motdMessage): ?>
 <blockquote><?=$this->batch($motdMessage['message'], 'escape|nl2br'); ?></blockquote>
 <?php endif; ?>
-<p class="banner"><?=$this->t('Welcome to eduVPN!'); ?></p>
+<p class="lead"><?=$this->t('Welcome to eduVPN!'); ?></p>
 <p>
     <?=$this->t('To use eduVPN, download the app for your device below!'); ?>
 </p>
 
-<div class="apps">
-	<a target="_blank" href="https://app.eduvpn.org/windows/eduVPNClient_latest.exe">
-		<img width="64" height="64" src="img/eduVPN/app/Windows.png" alt="Windows">
-		<span><?=$this->t('Windows'); ?></span>
-	</a>
-	<a target="_blank" href="https://apps.apple.com/app/eduvpn-client/id1317704208">
-		<img width="64" height="64" src="img/eduVPN/app/macOS.png" alt="macOS">
-		<span><?=$this->t('macOS'); ?></span>
-	</a>
-	<a target="_blank" href="https://play.google.com/store/apps/details?id=nl.eduvpn.app">
-		<img width="64" height="64" src="img/eduVPN/app/Android.png" alt="Android">
-		<span><?=$this->t('Android'); ?></span>
-	</a>
-	<a target="_blank" href="https://apps.apple.com/app/eduvpn-client/id1292557340">
-		<img width="64" height="64" src="img/eduVPN/app/iOS.png" alt="iOS">
-		<span><?=$this->t('iOS'); ?></span>
-	</a>
-</div> <!-- /apps -->
+<ul class="apps">
+    <li>
+        <a class="Windows" href="https://app.eduvpn.org/windows/eduVPNClient_latest.exe"><?=$this->t('Windows'); ?></a>
+    </li>
 
-<p class="small">
+    <li>
+        <a class="macOS" href="https://apps.apple.com/app/eduvpn-client/id1317704208"><?=$this->t('macOS'); ?></a>
+    </li>
+
+    <li>
+        <a class="Android" href="https://play.google.com/store/apps/details?id=nl.eduvpn.app"><?=$this->t('Android'); ?></a>
+    </li>
+
+    <li>
+        <a class="iOS" href="https://apps.apple.com/app/eduvpn-client/id1292557340"><?=$this->t('iOS'); ?></a>
+    </li>
+</ul>
+
+<p>
     <?=$this->t('On your "Account" page you can block access to the VPN in case you lose a device.'); ?>
     <?=$this->t('On the "Configurations" page, advanced users can manually download VPN configurations.'); ?>
 </p>
