e782096654916786caa8d58ea1d359190bb149fd
François Kooman <fkooman@tuxed.net>
2020-06-29 22:13:24 +0200

    add pl_PL translation

---
+++ locale/pl_PL.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2020, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Android' => 'Android',
+    'Continue...' => 'Kontynuuj...',
+    'Download and/or install the eduVPN application for your device as found on the "Home" page;' => 'Pobierz i zainstaluj aplikację eduVPN przeznaczoną dla Twojego urządzenia wybierając odpowiednią pozycję z listy na stronie głównej;',
+    'Getting Started' => 'Zaczynamy',
+    'If you have any problems or questions, please contact your institute.' => 'Jeżeli masz jakieś pytania lub pojawią się problemy, skontaktuj się ze swoją instytucją.',
+    'In order to start using eduVPN, follow the steps below.' => 'Aby zacząć używać eduVPNi, wykonaj następujące czynności.',
+    'Launch the eduVPN application and follow the instructions.' => 'Uruchom aplikację eduVPN i postępuj zgodnie z instrukcjami.',
+    'Linux' => 'Linux',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'Zaawansowani użytkownicy mogą ręcznie pobrać konfigurację VPN na stronie "Konfiguracja".',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'W przypadku utraty urządzenia możesz zablokować dostęp do VPN na stronie "Konto".',
+    'To use eduVPN, download the app for your device below!' => 'Aby używać eduVPN pobierz aplikację dla Twojego urządzenia!',
+    'Welcome to eduVPN!' => 'Witamy w eduVPN!',
+    'Windows' => 'Windows',
+    'iOS' => 'iOS',
+    'macOS' => 'macOS',
+];
