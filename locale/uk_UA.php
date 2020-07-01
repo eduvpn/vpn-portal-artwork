ab950955690040fb59d39faabe2257a85d666b20
Yevhenii <Melkin1968@users.noreply.github.com>
2020-07-01 15:58:19 +0300

    Add files via upload

---
+++ locale/uk_UA.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2020, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Android' => 'Android',
+    'Continue...' => 'Продовжувати',
+    'Download and/or install the eduVPN application for your device as found on the "Home" page;' => 'Завантажте та / або встановіть додаток eduVPN для свого пристрою, як показано на сторінці "Головна"',
+    'Getting Started' => 'Починаємо',
+    'If you have any problems or questions, please contact your institute.' => 'Якщо у вас є якісь проблеми або запитання, зверніться до своєї установи',
+    'In order to start using eduVPN, follow the steps below.' => 'Щоб почати використовувати eduVPN, виконайте наведені нижче кроки',
+    'Launch the eduVPN application and follow the instructions.' => 'Запустіть додаток eduVPN та дотримуйтесь інструкцій',
+    'Linux' => 'Linux',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'На сторінці "Конфігурації" досвідчені користувачі можуть завантажувати конфігурації VPN вручну',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'На вашій сторінці "Обліковий запис" ви можете заблокувати доступ до VPN у випадку втрати пристрою',
+    'To use eduVPN, download the app for your device below!' => 'Щоб використовувати eduVPN, завантажте додаток для свого пристрою, що знаходиться нижче!',
+    'Welcome to eduVPN!' => 'Ласкаво просимо до eduVPN!',
+    'Windows' => 'Windows',
+    'iOS' => 'iOS',
+    'macOS' => 'macOS',
+];
