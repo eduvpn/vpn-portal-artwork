6f67c212c831fce6bffb34d29842844714fceb88
François Kooman <fkooman@tuxed.net>
2020-08-12 15:57:55 +0200

    Tpl::getCssUrl -> Tpl::getAssetUrl

--- views/base.php
+++ views/base.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>eduVPN - <?=$this->e($pageTitle); ?></title>
-    <link href="<?=$this->getCssUrl($requestRoot, 'screen.css'); ?>" media="screen" rel="stylesheet">
-    <link href="<?=$this->getCssUrl($requestRoot, 'eduVPN/screen.css'); ?>" media="screen" rel="stylesheet">
+    <link href="<?=$this->getAssetUrl($requestRoot, 'css/screen.css'); ?>" media="screen" rel="stylesheet">
+    <link href="<?=$this->getAssetUrl($requestRoot, 'css/eduVPN/screen.css'); ?>" media="screen" rel="stylesheet">
 </head>
 <body>
     <header class="page">
