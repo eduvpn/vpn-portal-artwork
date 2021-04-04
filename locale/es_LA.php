f4627519552267d721dd7f74f719ec5ad4cf930b
Kako Pedreros <kakopedreros@users.noreply.github.com>
2021-04-03 21:13:21 -0300

    add es_LA.php

---
+++ locale/es_LA.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * eduVPN - End-user friendly VPN.
+ *
+ * Copyright: 2016-2020, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Android' => '',
+    'Continue...' => '',
+    'Download and/or install the eduVPN application for your device as found on the "Home" page;' => '',
+    'Getting Started' => '',
+    'If you have any problems or questions, please contact your institute.' => '',
+    'In order to start using eduVPN, follow the steps below.' => '',
+    'Launch the eduVPN application and follow the instructions.' => '',
+    'Linux' => '',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => '',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => '',
+    'To use eduVPN, download the app for your device below!' => '',
+    'Welcome to eduVPN!' => '',
+    'Windows' => '',
+    'iOS' => '',
+    'macOS' => '',
+];
