0e2c8e9d55a766cb84065c7bb866d1631a9808a8
Kako Pedreros <kakopedreros@users.noreply.github.com>
2021-04-03 21:21:31 -0300

    tranlation to spanish

--- locale/es_LA.php
+++ locale/es_LA.php
@@ -3,24 +3,24 @@
 /*
  * eduVPN - End-user friendly VPN.
  *
- * Copyright: 2016-2020, The Commons Conservancy eduVPN Programme
+ * Copyright: 2016-2021, The Commons Conservancy eduVPN Programme
  * SPDX-License-Identifier: AGPL-3.0+
  */
 
 return [
-    'Android' => '',
-    'Continue...' => '',
-    'Download and/or install the eduVPN application for your device as found on the "Home" page;' => '',
-    'Getting Started' => '',
-    'If you have any problems or questions, please contact your institute.' => '',
-    'In order to start using eduVPN, follow the steps below.' => '',
-    'Launch the eduVPN application and follow the instructions.' => '',
-    'Linux' => '',
-    'On the "Configurations" page, advanced users can manually download VPN configurations.' => '',
-    'On your "Account" page you can block access to the VPN in case you lose a device.' => '',
-    'To use eduVPN, download the app for your device below!' => '',
-    'Welcome to eduVPN!' => '',
-    'Windows' => '',
-    'iOS' => '',
-    'macOS' => '',
+    'Android' => 'Android',
+    'Continue...' => 'Continuar...',
+    'Download and/or install the eduVPN application for your device as found on the "Home" page;' => 'Descargue y/o instale la aplicación eduVPN para su dispositivo como se encuentra en la página "Inicio";',
+    'Getting Started' => 'Empezando',
+    'If you have any problems or questions, please contact your institute.' => 'Si tiene algún problema o pregunta, comuníquese con su institución.',
+    'In order to start using eduVPN, follow the steps below.' => 'Para comenzar a usar eduVPN, siga los pasos a continuación.',
+    'Launch the eduVPN application and follow the instructions.' => 'Inicie la aplicación eduVPN y siga las instrucciones.',
+    'Linux' => 'Linux',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'En la página "Configuraciones", los usuarios avanzados pueden descargar manualmente configuraciones de VPN.',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'En su página "Cuenta" puede bloquear el acceso a la VPN en caso de que pierda un dispositivo.',
+    'To use eduVPN, download the app for your device below!' => 'Para usar eduVPN, descargue la aplicación para su dispositivo a continuación!',
+    'Welcome to eduVPN!' => 'Bienvenido a eduVPN!',
+    'Windows' => 'Windows',
+    'iOS' => 'iOS',
+    'macOS' => 'macOS',
 ];
