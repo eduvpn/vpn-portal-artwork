81aa58522311d18e4de13382f37e83d1cd140de1
roman <roman.mariancik@gmail.com>
2023-08-10 09:09:28 +0200

    Added Czech translation

---
+++ locale/cs_CZ.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * eduVPN - Uživatelsky přívětivá VPN.
+ *
+ * Copyright: 2016-2021, The Commons Conservancy eduVPN Programme
+ * SPDX-License-Identifier: AGPL-3.0+
+ */
+
+return [
+    'Android' => 'Android',
+    'Continue...' => 'Pokračovat...',
+    'Download and/or install the eduVPN application for your device as found on the "Home" page;' => 'Stáhněte a/nebo nainstalujte aplikaci eduVPN pro vaše zařízení podle instrukcí na stránce "Domů";',
+    'Getting Started' => 'Začínáme',
+    'If you have any problems or questions, please contact your institute.' => 'Pokud máte nějaké problémy nebo otázky, kontaktujte prosím svůj institut.',
+    'In order to start using eduVPN, follow the steps below.' => 'Abyste mohli začít používat eduVPN, postupujte podle následujících kroků.',
+    'Launch the eduVPN application and follow the instructions.' => 'Spusťte aplikaci eduVPN a postupujte podle pokynů.',
+    'Linux' => 'Linux',
+    'On the "Configurations" page, advanced users can manually download VPN configurations.' => 'Na stránce "Konfigurace" mohou pokročilí uživatelé manuálně stáhnout VPN konfigurace.',
+    'On your "Account" page you can block access to the VPN in case you lose a device.' => 'Na vaší stránce "Účet" můžete blokovat přístup k VPN v případě, že ztratíte zařízení.',
+    'To use eduVPN, download the app for your device below!' => 'Pro použití eduVPN stáhněte aplikaci pro své zařízení níže!',
+    'Welcome to eduVPN!' => 'Vítejte v eduVPN!',
+    'Windows' => 'Windows',
+    'iOS' => 'iOS',
+    'macOS' => 'macOS',
+];
